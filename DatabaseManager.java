efc4e9617970a0cd43cb8fbbe40d43c11c45e653
Chris Schweinhart <schwein@vt.edu>
2013-03-30 19:20:26 -0400

    Complete database manager

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.LinkedList;
@@ -16,22 +17,33 @@ public class DatabaseManager {
 	 * with given offsets and lengths.
 	 */
 	RandomAccessFile file;
-	
+
 	/**
 	 * Linked List for keeping track of all free memory
 	 * blocks.  Each block is represented by a Handle,
 	 * with a given offset and length.
 	 */
 	LinkedList<Handle> free;
-	
+
 	/**
 	 * Basic constructor for the DatabaseManager class.
 	 * Will initialize all member fields appropriately.
 	 */
 	public DatabaseManager() {
-		// TODO implement constructor
+		try {
+			file = new RandomAccessFile("biofile.bin", "rw");
+			// Make sure we are overwriting file.
+			file.setLength(0);
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not find/create file.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Could not overwrite file.");
+			e.printStackTrace();
+		}
+		free = new LinkedList<Handle>();
 	}
-	
+
 	/**
 	 * Method to insert a given sequence into the first
 	 * free memory block.  If there are no free memory
@@ -43,16 +55,11 @@ public class DatabaseManager {
 	 * @return - the Handle for the given sequence
 	 */
 	public Handle insert(String sequence, int length) {
-		
 		// Calculate number of bytes needed to store this sequence
-		int bytesNeeded = (int) Math.ceil(length / 4);
-		
-		// Check if we can use a free block
+		int bytesNeeded = (int) Math.ceil((double)(length / 4.0));
 		for (Handle freeBlock: free) {
 			int offset = freeBlock.getOffset();
 			if (freeBlock.getLength() >= bytesNeeded) {
-				
-				// Attempt to write to our data file
 				try {
 					file.seek(offset);
 					file.write(buildByteArray(sequence, bytesNeeded));
@@ -61,126 +68,110 @@ public class DatabaseManager {
 					e.printStackTrace();
 					return null;
 				}
-				
-				// Fix the list of free bytes according to how much we used
 				if (freeBlock.getLength() == bytesNeeded) {
 					free.remove(freeBlock);
-				} else {
+				}
+				else {
 					free.set(free.indexOf(freeBlock), new Handle(offset + bytesNeeded, freeBlock.getLength() - bytesNeeded));
 				}
-				
 				return new Handle(offset, bytesNeeded);
 			}
 		}
-		
+		// No valid free space so append to end of file
 		int oldLength = -1;
 		try {
 			oldLength = (int) file.length();
-		} catch (IOException e) {
-			System.err.println("Problem writing to file. See stack trace for details.");
-			e.printStackTrace();
-		}
-		
-		// Check if the last free block is at the end of file
-		Handle last = free.getLast();
-		if (last.getOffset() + last.getLength() == oldLength) {
-			
-			// Attempt to write to our data file
-			int offset = last.getOffset();
-			try {
-				file.seek(offset);
-				file.write(buildByteArray(sequence, bytesNeeded));
-			} catch (IOException e) {
-				System.err.println("Problem writing to file. See stack trace for details.");
-				e.printStackTrace();
-				return null;
+			Handle fb = freeBlockAtEnd();
+			if (fb != null) {
+				// If free block at end, extend length to only amount we need and remove free block
+				file.setLength(fb.getOffset() + bytesNeeded);
+				file.seek(fb.getOffset());
+				free.remove(fb);
 			}
-			
-			// Remove the last block we used up
-			free.remove(last);
-			
-			return new Handle(offset, bytesNeeded);
-		}
-		
-		// No valid free space so append to end of file
-		try {
-			file.setLength(oldLength + bytesNeeded);
-			file.seek(oldLength);
+			else {
+				file.setLength(oldLength + bytesNeeded);
+				file.seek(oldLength);
+			}
 			file.write(buildByteArray(sequence, bytesNeeded));
 		} catch (IOException e) {
 			System.err.println("Problem writing to file. See stack trace for details.");
 			e.printStackTrace();
 		}
-		
 		return new Handle(oldLength, bytesNeeded);
 	}
-	
+
+	/**
+	 * Determines if there is a free block of memory at the end of the file.
+	 * @return the Handle object of the free block at the end of the file, or null if none
+	 */
+	private Handle freeBlockAtEnd() {
+		for (Handle h: free) {
+			try {
+				if (file.length() - h.getLength() == h.getOffset()) {
+					return h;
+				}
+			} catch (IOException e) {
+				System.err.println("Could not find file length.");
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
 	/**
 	 * Builds a byte array of the given sequence represented in binary.
-	 * 
 	 * @param sequence - the String sequence to build from
 	 * @param bytesNeeded - the number of bytes needed to represent the sequence
-	 * @return - a byte array that should be written to the file
+	 * @return a byte array that should be written to the file
 	 */
 	private byte[] buildByteArray(String sequence, int bytesNeeded) {
-		
-		// Append A's to the sequence to make it even
-		for (int mod = sequence.length() % 4; mod > 0; mod--) {
-			sequence += "A";
-		}
-		
-		// Set each byte to the appropriate string of 4 characters
 		byte[] array = new byte[bytesNeeded];
-		for (int i = 0; i < bytesNeeded; i++) {
-			array[i] = stringtobyte(sequence.substring(i, i + 4));
+		int currentByte = 0, count = 0;
+		for (int i = 0; i < sequence.length(); i++) {
+			int mod = i % 4;
+			switch (mod) {
+			case 0:
+				currentByte = (getCharValue(sequence.charAt(i))) << 6;
+				break;
+			case 1:
+				currentByte |= (getCharValue(sequence.charAt(i))) << 4;
+				break;
+			case 2:
+				currentByte |= (getCharValue(sequence.charAt(i))) << 2;
+				break;
+			case 3:
+				currentByte |= getCharValue(sequence.charAt(i));
+				array[count] = (byte) currentByte;
+				count++;
+				break;
+			}
 		}
-		
+		// Makes sure we set the last byte, in case 4 does not divide sequence.length()
+		if (count == bytesNeeded - 1) {
+			array[count] = (byte) currentByte;
+		}
 		return array;
 	}
-	
+
 	/**
-	 * Converts a string of four ACGT characters into a byte
-	 * for storage in data memory.
-	 * 
-	 * @param sequence - a sequence of four ACGT characters
-	 * @return - an appropriate byte for the four characters
-	 */
-	private byte stringtobyte(String sequence) {
-		sequence = sequence.toUpperCase();
-		byte output = 0;
-	
-		// Shift left twice and add 2 new bits
-		for (char c : sequence.toCharArray()) {
-			output <<= 2;
-			output |= getCharValue(c);
-		}
-		
-		return output;
-	}
-	
-	/**
-	 * Method to determine the correct binary value for a given
-	 * ACGT character.
-	 * 
+	 * Returns the binary representation for the given character.
 	 * @param c - the character to convert to binary
-	 * @return - the binary value of the given character
+	 * @return the binary value of the given character
 	 */
-	private byte getCharValue(char c) {
+	private int getCharValue(char c) {
 		c = Character.toUpperCase(c);
-		
 		if (c == 'A')
-			return 0;
+			return 0b00;
 		if (c == 'C')
-			return 1;
+			return 0b01;
 		if (c == 'G')
-			return 2;
+			return 0b10;
 		if (c == 'T')
-			return 3;
-		
+			return 0b11;
 		System.err.println(c + " is not a valid character for this sequence.");
 		return -1;
 	}
-	
+
 	/**
 	 * Method to remove a sequence from the database.
 	 * Creates a new free memory block in the place of
@@ -189,9 +180,31 @@ public class DatabaseManager {
 	 * @param handle - the given Handle for the sequence
 	 */
 	public void remove(Handle handle) {
-		// TODO implement remove
+		for (Handle h: free) {
+			if (handle.getOffset() < h.getOffset()) {
+				free.add(free.indexOf(h), handle);
+				mergeFreeBlocks();
+				return;
+			}
+		}
+		free.add(handle);
 	}
-	
+
+	/**
+	 * Merges adjacent free memory blocks together.
+	 */
+	private void mergeFreeBlocks() {
+		for (int i = 0; i < free.size() - 1; i++) {
+			Handle current = free.get(i), next = free.get(i+1);
+			if (current.getOffset() + current.getLength() == next.getOffset()) {
+				int newLength = current.getLength() + next.getLength();
+				free.remove(next);
+				free.set(i, new Handle(current.getOffset(), newLength));
+				i--;
+			}
+		}
+	}
+
 	/**
 	 * Method to retrieve a DNA sequence using a given
 	 * handle.  Will give the bytes in memory regardless
@@ -202,11 +215,42 @@ public class DatabaseManager {
 	 * @return - the sequence in the memory location
 	 */
 	public String getEntry(Handle handle) {
-		// TODO implement getEntry
-		
-		return "NYI: getEntry(Handle handle)";
+		byte[] bytes = new byte[handle.getLength()];
+		try {
+			file.seek(handle.getOffset());
+			file.read(bytes);
+		} catch (IOException e) {
+			System.err.println("Cannot read byte sequence for given handle.");
+			e.printStackTrace();
+		}
+		String result = "";
+		for (byte b: bytes) {
+			result += getStrFromBin(b);
+		}
+		return result;
 	}
-	
+
+	/**
+	 * Returns the binary representation for the given character.
+	 * @param c - the character to convert to binary
+	 * @return the binary value of the given character
+	 */
+	private String getStrFromBin(byte b) {
+		int[] charsInByte = {(b & 0xC0) >> 6, (b & 0x30) >> 4, (b & 0x0C) >> 2, (b & 0x03)};
+		String res = "";
+		for (int c: charsInByte) {
+			if (c == 0)
+				res += "A";
+			else if (c == 1)
+				res += "C";
+			else if (c == 2)
+				res += "G";
+			else if (c == 3)
+				res += "T";
+		}
+		return res;
+	}
+
 	/**
 	 * Method to produce a string representation of all
 	 * free memory blocks.
@@ -214,8 +258,10 @@ public class DatabaseManager {
 	 * @return - all elements of the linked list free
 	 */
 	public String toString() {
-		// TODO implement toString
-		
-		return "NYI: toString()";
+		String ret = "";
+		for (Handle h: free) {
+			ret += h.toString();
+		}
+		return ret.isEmpty() ? "No free memory blocks.": ret;
 	}
 }
---
+++ OldDBM.java
@@ -0,0 +1,221 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.LinkedList;
+
+/**
+ * Database manager for keeping track of sequence memory
+ * and free blocks.  Allows for several interface methods
+ * using the Handle class to determine which bytes are
+ * sequences.
+ */
+public class OldDBM {
+
+	/**
+	 * File pointer to our byte array on disk.  Used to
+	 * store and access sequences based on give Handles,
+	 * with given offsets and lengths.
+	 */
+	RandomAccessFile file;
+	
+	/**
+	 * Linked List for keeping track of all free memory
+	 * blocks.  Each block is represented by a Handle,
+	 * with a given offset and length.
+	 */
+	LinkedList<Handle> free;
+	
+	/**
+	 * Basic constructor for the DatabaseManager class.
+	 * Will initialize all member fields appropriately.
+	 */
+	public OldDBM() {
+		// TODO implement constructor
+	}
+	
+	/**
+	 * Method to insert a given sequence into the first
+	 * free memory block.  If there are no free memory
+	 * blocks of sufficient size, will create a new one
+	 * and add it to the end of the file.
+	 * 
+	 * @param sequence - the sequence to insert
+	 * @param length - the length of the given sequence
+	 * @return - the Handle for the given sequence
+	 */
+	public Handle insert(String sequence, int length) {
+		
+		// Calculate number of bytes needed to store this sequence
+		int bytesNeeded = (int) Math.ceil(length / 4);
+		
+		// Check if we can use a free block
+		for (Handle freeBlock: free) {
+			int offset = freeBlock.getOffset();
+			if (freeBlock.getLength() >= bytesNeeded) {
+				
+				// Attempt to write to our data file
+				try {
+					file.seek(offset);
+					file.write(buildByteArray(sequence, bytesNeeded));
+				} catch (IOException e) {
+					System.err.println("Problem writing to file. See stack trace for details.");
+					e.printStackTrace();
+					return null;
+				}
+				
+				// Fix the list of free bytes according to how much we used
+				if (freeBlock.getLength() == bytesNeeded) {
+					free.remove(freeBlock);
+				} else {
+					free.set(free.indexOf(freeBlock), new Handle(offset + bytesNeeded, freeBlock.getLength() - bytesNeeded));
+				}
+				
+				return new Handle(offset, bytesNeeded);
+			}
+		}
+		
+		int oldLength = -1;
+		try {
+			oldLength = (int) file.length();
+		} catch (IOException e) {
+			System.err.println("Problem writing to file. See stack trace for details.");
+			e.printStackTrace();
+		}
+		
+		// Check if the last free block is at the end of file
+		Handle last = free.getLast();
+		if (last.getOffset() + last.getLength() == oldLength) {
+			
+			// Attempt to write to our data file
+			int offset = last.getOffset();
+			try {
+				file.seek(offset);
+				file.write(buildByteArray(sequence, bytesNeeded));
+			} catch (IOException e) {
+				System.err.println("Problem writing to file. See stack trace for details.");
+				e.printStackTrace();
+				return null;
+			}
+			
+			// Remove the last block we used up
+			free.remove(last);
+			
+			return new Handle(offset, bytesNeeded);
+		}
+		
+		// No valid free space so append to end of file
+		try {
+			file.setLength(oldLength + bytesNeeded);
+			file.seek(oldLength);
+			file.write(buildByteArray(sequence, bytesNeeded));
+		} catch (IOException e) {
+			System.err.println("Problem writing to file. See stack trace for details.");
+			e.printStackTrace();
+		}
+		
+		return new Handle(oldLength, bytesNeeded);
+	}
+	
+	/**
+	 * Builds a byte array of the given sequence represented in binary.
+	 * 
+	 * @param sequence - the String sequence to build from
+	 * @param bytesNeeded - the number of bytes needed to represent the sequence
+	 * @return - a byte array that should be written to the file
+	 */
+	private byte[] buildByteArray(String sequence, int bytesNeeded) {
+		
+		// Append A's to the sequence to make it even
+		for (int mod = sequence.length() % 4; mod > 0; mod--) {
+			sequence += "A";
+		}
+		
+		// Set each byte to the appropriate string of 4 characters
+		byte[] array = new byte[bytesNeeded];
+		for (int i = 0; i < bytesNeeded; i++) {
+			array[i] = stringtobyte(sequence.substring(i, i + 4));
+		}
+		
+		return array;
+	}
+	
+	/**
+	 * Converts a string of four ACGT characters into a byte
+	 * for storage in data memory.
+	 * 
+	 * @param sequence - a sequence of four ACGT characters
+	 * @return - an appropriate byte for the four characters
+	 */
+	private byte stringtobyte(String sequence) {
+		sequence = sequence.toUpperCase();
+		byte output = 0;
+	
+		// Shift left twice and add 2 new bits
+		for (char c : sequence.toCharArray()) {
+			output <<= 2;
+			output |= getCharValue(c);
+		}
+		
+		return output;
+	}
+	
+	/**
+	 * Method to determine the correct binary value for a given
+	 * ACGT character.
+	 * 
+	 * @param c - the character to convert to binary
+	 * @return - the binary value of the given character
+	 */
+	private byte getCharValue(char c) {
+		c = Character.toUpperCase(c);
+		
+		if (c == 'A')
+			return 0;
+		if (c == 'C')
+			return 1;
+		if (c == 'G')
+			return 2;
+		if (c == 'T')
+			return 3;
+		
+		System.err.println(c + " is not a valid character for this sequence.");
+		return -1;
+	}
+	
+	/**
+	 * Method to remove a sequence from the database.
+	 * Creates a new free memory block in the place of
+	 * the removed sequence.
+	 * 
+	 * @param handle - the given Handle for the sequence
+	 */
+	public void remove(Handle handle) {
+		// TODO implement remove
+	}
+	
+	/**
+	 * Method to retrieve a DNA sequence using a given
+	 * handle.  Will give the bytes in memory regardless
+	 * of whether or not they have meaning (i.e. has no
+	 * error checking).
+	 * 
+	 * @param handle - the given Handle for the sequence
+	 * @return - the sequence in the memory location
+	 */
+	public String getEntry(Handle handle) {
+		// TODO implement getEntry
+		
+		return "NYI: getEntry(Handle handle)";
+	}
+	
+	/**
+	 * Method to produce a string representation of all
+	 * free memory blocks.
+	 * 
+	 * @return - all elements of the linked list free
+	 */
+	public String toString() {
+		// TODO implement toString
+		
+		return "NYI: toString()";
+	}
+}
