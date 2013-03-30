c343c4f46dfafd6617573711eab5b3644e90efd6
Chris Schweinhart <schwein@vt.edu>
2013-03-30 17:13:09 -0400

    Changes to insert and generalization of build byte array

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -43,11 +43,16 @@ public class DatabaseManager {
 	 * @return - the Handle for the given sequence
 	 */
 	public Handle insert(String sequence, int length) {
+		
 		// Calculate number of bytes needed to store this sequence
 		int bytesNeeded = (int) Math.ceil(length / 4);
+		
+		// Check if we can use a free block
 		for (Handle freeBlock: free) {
 			int offset = freeBlock.getOffset();
 			if (freeBlock.getLength() >= bytesNeeded) {
+				
+				// Attempt to write to our data file
 				try {
 					file.seek(offset);
 					file.write(buildByteArray(sequence, bytesNeeded));
@@ -56,20 +61,49 @@ public class DatabaseManager {
 					e.printStackTrace();
 					return null;
 				}
+				
+				// Fix the list of free bytes according to how much we used
 				if (freeBlock.getLength() == bytesNeeded) {
 					free.remove(freeBlock);
-				}
-				else {
+				} else {
 					free.set(free.indexOf(freeBlock), new Handle(offset + bytesNeeded, freeBlock.getLength() - bytesNeeded));
 				}
+				
 				return new Handle(offset, bytesNeeded);
 			}
 		}
-		// No valid free space so append to end of file
-		//TODO: Improve this. What about if we have a small free block at the end of the file? Extend only amount necessary.
+		
 		int oldLength = -1;
 		try {
 			oldLength = (int) file.length();
+		} catch (IOException e) {
+			System.err.println("Problem writing to file. See stack trace for details.");
+			e.printStackTrace();
+		}
+		
+		// Check if the last free block is at the end of file
+		Handle last = free.getLast();
+		if (last.getOffset() + last.getLength() == oldLength) {
+			
+			// Attempt to write to our data file
+			int offset = last.getOffset();
+			try {
+				file.seek(offset);
+				file.write(buildByteArray(sequence, bytesNeeded));
+			} catch (IOException e) {
+				System.err.println("Problem writing to file. See stack trace for details.");
+				e.printStackTrace();
+				return null;
+			}
+			
+			// Remove the last block we used up
+			free.remove(last);
+			
+			return new Handle(offset, bytesNeeded);
+		}
+		
+		// No valid free space so append to end of file
+		try {
 			file.setLength(oldLength + bytesNeeded);
 			file.seek(oldLength);
 			file.write(buildByteArray(sequence, bytesNeeded));
@@ -77,57 +111,72 @@ public class DatabaseManager {
 			System.err.println("Problem writing to file. See stack trace for details.");
 			e.printStackTrace();
 		}
+		
 		return new Handle(oldLength, bytesNeeded);
 	}
 	
 	/**
 	 * Builds a byte array of the given sequence represented in binary.
+	 * 
 	 * @param sequence - the String sequence to build from
 	 * @param bytesNeeded - the number of bytes needed to represent the sequence
-	 * @return a byte array that should be written to the file
+	 * @return - a byte array that should be written to the file
 	 */
 	private byte[] buildByteArray(String sequence, int bytesNeeded) {
-		byte[] array = new byte[bytesNeeded];
-		int currentByte = 0, count = 0;
-		for (int i = 0; i < sequence.length(); i++) {
-			int mod = sequence.length() % 4;
-			switch (mod) {
-			case 0:
-				currentByte = (getCharValue(sequence.charAt(i))) << 3;
-				break;
-			case 1:
-				currentByte |= (getCharValue(sequence.charAt(i))) << 2;
-				break;
-			case 2:
-				currentByte |= (getCharValue(sequence.charAt(i))) << 1;
-				break;
-			case 3:
-				currentByte |= getCharValue(sequence.charAt(i));
-				array[count] = (byte) currentByte;
-				count++;
-				break;
-			}
+		
+		// Append A's to the sequence to make it even
+		for (int mod = sequence.length() % 4; mod > 0; mod--) {
+			sequence += "A";
 		}
-		// Makes sure we set the last byte, in case 4 does not divide sequence.length()
-		array[count] = (byte) currentByte;
+		
+		// Set each byte to the appropriate string of 4 characters
+		byte[] array = new byte[bytesNeeded];
+		for (int i = 0; i < bytesNeeded; i++) {
+			array[i] = stringtobyte(sequence.substring(i, i + 4));
+		}
+		
 		return array;
 	}
 	
 	/**
-	 * Returns the binary representation for the given character.
+	 * Converts a string of four ACGT characters into a byte
+	 * for storage in data memory.
+	 * 
+	 * @param sequence - a sequence of four ACGT characters
+	 * @return - an appropriate byte for the four characters
+	 */
+	private byte stringtobyte(String sequence) {
+		sequence = sequence.toUpperCase();
+		byte output = 0;
+	
+		// Shift left twice and add 2 new bits
+		for (char c : sequence.toCharArray()) {
+			output <<= 2;
+			output |= getCharValue(c);
+		}
+		
+		return output;
+	}
+	
+	/**
+	 * Method to determine the correct binary value for a given
+	 * ACGT character.
+	 * 
 	 * @param c - the character to convert to binary
-	 * @return the binary value of the given character
+	 * @return - the binary value of the given character
 	 */
 	private byte getCharValue(char c) {
 		c = Character.toUpperCase(c);
+		
 		if (c == 'A')
 			return 0;
 		if (c == 'C')
 			return 1;
 		if (c == 'G')
-			return 10;
+			return 2;
 		if (c == 'T')
-			return 11;
+			return 3;
+		
 		System.err.println(c + " is not a valid character for this sequence.");
 		return -1;
 	}
